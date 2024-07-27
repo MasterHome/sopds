9321d9665485cfcdcfeeb09a6b76b78578711945
MasterHome <62955616+MasterHome@users.noreply.github.com>
2024-07-27 17:23:54 +0500

    Update settings.py

--- sopds/settings.py
+++ sopds/settings.py
@@ -14,7 +14,7 @@ import os
 import sys
 from collections import OrderedDict
 
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -106,7 +106,7 @@ WSGI_APPLICATION = 'sopds.wsgi.application'
 #    'NAME': 'sopds',
 #    'USER': 'sopds',
 #    'PASSWORD': 'sopds',
-#    'HOST': '', # Set to empty string for localhost.
+#    'HOST': 'localhost', # Set to empty string for localhost.
 #    'PORT': '', # Set to empty string for default.
 #    }
 #}
